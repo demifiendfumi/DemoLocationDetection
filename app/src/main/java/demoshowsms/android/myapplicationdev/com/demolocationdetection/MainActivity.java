32fd3a20e648acc35fe2cfa0ef15d50b428bb6fc
16022603 <16022603@myrp.edu.sg>
2018-07-17 16:30:12 +0800

    first commit

---
+++ app/src/main/java/demoshowsms/android/myapplicationdev/com/demolocationdetection/MainActivity.java
@@ -0,0 +1,110 @@
+package demoshowsms.android.myapplicationdev.com.demolocationdetection;
+
+import android.Manifest;
+import android.location.Location;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLastLocation, btnUpdateLocation, btnRemoveLocation;
+    FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLastLocation = (Button)findViewById(R.id. btnGetLastLocation);
+        btnUpdateLocation = (Button)findViewById(R.id. btnGetLocationUpdate);
+        btnRemoveLocation = (Button)findViewById(R.id. btnRemoveLocationUpdate);
+
+
+        client = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        final LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setSmallestDisplacement(100);
+
+        final LocationCallback mLocationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult){
+                if(locationResult != null){
+                    Location data = locationResult.getLastLocation();
+                    double lat = data.getLatitude();
+                    double lng = data.getLongitude();
+                }
+            }
+        };
+
+        btnLastLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkPermission() == true){
+                    Task<Location> task = client.getLastLocation();
+                    task.addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if(location!=null){
+                                String msg = "Lat : " + location.getLatitude() +
+                                        "Lng : " + location.getLongitude();
+                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                            }else{
+                                String msg = "No Last Known Location found";
+                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+        btnUpdateLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkPermission() == true){
+                    client.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+                }
+
+
+            }
+        });
+
+        btnRemoveLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private boolean checkPermission(){
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED || permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
